52a5bcd08eaa2cff2588782c31f6375f206779cd
伊東和成 <itokazunari5375@icloud.com>
2022-04-30 23:12:27 +0900

    不要部分の削除

--- __tests__/BlogPage.test.tsx
+++ __tests__/BlogPage.test.tsx
@@ -35,49 +35,4 @@ const handlers = [
       )
     }
   }),
-  // rest.get(
-  //   'https://jsonplaceholder.typicode.com/posts/?_limit=10',
-  //   (req, res, ctx) => {
-  //     return res(
-  //       ctx.status(200),
-  //       ctx.json([
-  //         {
-  //           userId: 1,
-  //           id: 1,
-  //           title: 'dummy title 1',
-  //           body: 'dummy body 1',
-  //         },
-  //         {
-  //           userId: 2,
-  //           id: 2,
-  //           title: 'dummy title 2',
-  //           body: 'dummy body 2',
-  //         },
-  //       ])
-  //     )
-  //   }
-  // ),
 ]
-const server = setupServer(...handlers)
-beforeAll(() => {
-  server.listen()
-})
-afterEach(() => {
-  server.resetHandlers()
-  cleanup()
-})
-afterAll(() => {
-  server.close()
-})
-
-describe(`Blog page`, () => {
-  it('Should render the list of blogs pre-fetched by getStaticProps', async () => {
-    const { page } = await getPage({
-      route: '/blog-page',
-    })
-    render(page)
-    expect(await screen.findByText('blog page')).toBeInTheDocument()
-    expect(screen.getByText('dummy title 1')).toBeInTheDocument()
-    expect(screen.getByText('dummy title 2')).toBeInTheDocument()
-  })
-})
